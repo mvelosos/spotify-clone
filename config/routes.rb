6c3d6eae0b9eda7a57f9b2be42446b309d6feed2
Mateus Veloso <mfvelosos@gmail.com>
2018-11-28 02:47:04 +0000

    Album controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'albums/show'
   #get 'artists/show'
   #get 'categories/show'
   devise_for :users
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
     resources :search, only: [:index, :new], as: :searches
     resources :categories, only: :show
     resources :artists, only: :show
+    resources :albums, only: :show
   end
  
   unauthenticated :user do
